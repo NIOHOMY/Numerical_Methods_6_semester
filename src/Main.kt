ea2c48f2050c20803449e598abe316d68aed3d3c
N1oH0my <h0myn1o@mail.ru>
2024-05-31 15:10:50 +0300

    fixed solution

--- src/Main.kt
+++ src/Main.kt
@@ -5,10 +5,11 @@ import entities.Solver
 fun main() {
     val dataReader = DataReader("C://Users//N1o//0projects//IntellijIdea//kt//Numerical_Methods_6_semester//src//res//data.txt")
     val data = dataReader.getData()
-    val solver = Solver(data,
-        p = "0.2",
+    val solver = Solver(
+        data,
+        p = "0",
         q = "0",
-        f = "2x",
+        f = "(6*x)",
     )
     solver.solve()
 }
